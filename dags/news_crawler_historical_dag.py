0496a885805e249abaeb7e5e6462fefb40c67ba4
Mustufain Abbas <mustufain.abbas@vertoanalytics.com>
2021-06-12 01:30:28 +0300

    add dag to process historical data

--- dags/news_crawler_historical_dag.py
+++ dags/news_crawler_historical_dag.py
@@ -4,23 +4,28 @@ from airflow.providers.amazon.aws.operators.ecs import ECSOperator
 from airflow.models import Variable
 
 
-start_date = date(2021, 6, 1)  # 1 June 2021
+start_date = date(2014, 1, 1)  # 1st Jan 2014
+end_date = date(2021, 5, 31)  # 31st May 2021
 start_datetime = datetime.combine(start_date,
-                                  datetime.min.time())
+                                      datetime.min.time())
+end_datetime = datetime.combine(end_date,
+                                datetime.min.time())
+
 default_args = {
     'retries': 2,
     'retry_delay': timedelta(minutes=2),
-    'start_date': start_datetime
+    'start_date': start_datetime,
+    'end_date': end_datetime
 }
 
-NEWS_CRAWLER_DAG = DAG(dag_id='news_crawler',
+NEWS_CRAWLER_DAG = DAG(dag_id='news_crawler_historical',
                        schedule_interval='@daily',  # every day at midnight
                        default_args=default_args,
-                       concurrency=3,
+                       concurrency=2,
                        catchup=True)
 
 
-SCRAPER = ECSOperator(task_id='news_scraper',
+SCRAPER = ECSOperator(task_id='news_scraper_historical',
                       dag=NEWS_CRAWLER_DAG,
                       # set this connection id in airflow web UI
                       aws_conn_id="aws_default",
